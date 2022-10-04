04f502d10b03c79bb8f0e06b73029b89521fdf6a
bebeloper <kevind171819@gmail.com>
2022-10-03 23:09:55 -0500

    Card added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,43 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+
   title = 'my-store';
+  btnAddToggle: boolean = true;
+  // backgroundLiMarket: String = '';
+
   persons = [{
     avatarPic: "https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg",
     firstName: "Kevind",
     lastName: "Ospina",
     age: 28,
-    eyeColor: "Green"
+    eyeColor: "Green",
+    added: true
   },
   {
     avatarPic: "https://img.freepik.com/premium-vector/female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-617.jpg",
     firstName: "Mayra",
     lastName: "Ospina",
     age: 26,
-    eyeColor: "Brown"
+    eyeColor: "Brown",
+    added: true
   }];
 
   market : any[] = [{
     productName: 'Meat',
     quantity: 1,
     productImg: '../assets/products/carne.png',
-    price: 12000
+    price: 12000,
+    added: false,
+    productIndex: 1
   },
   {
     productName: 'Chicken',
     quantity: 1,
     productImg: '../assets/products/chicken-leg.png',
-    price: 8500
+    price: 8500,
+    added: false,
+    productIndex: 1
   }];
 
   miniCart: any[] = [];
@@ -52,10 +62,19 @@ export class AppComponent {
   }
 
   addMiniCart(index: number){
+    this.market[index].added = true;
+    this.market[index].productIndex = index;
     this.miniCart.push(this.market[index]);
   }
 
   deleteItemsFromCart(index: number) {
+    console.log(this.miniCart[index].added);
+    console.log('Market added: ' + this.market[this.miniCart[index].productIndex].added);
+    this.market[this.miniCart[index].productIndex].added = false;
     this.miniCart.splice(index, 1);
   }
+
+  // productAdded(){
+  //   this.btnAddToggle = false;
+  // }
 }
